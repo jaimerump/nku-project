99dc3e9d7e402a76c3b9abc855375f4d01ce6e3d
Jaime Rump <rumpj2@nku.edu>
2014-04-03 18:53:18 +0000

    Set up the html/js inclusions

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# NKU Project Repo
+# CompleteSet Rails Repo
 
 CompleteSet is the treasure map for collectors.
+
+# Test Procedure
+
+You can log in at http://rumpj2-72842.use1.nitrousbox.com/login using the test account with email/password test@completeset.com/testing.
+
---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,74 @@
+//Header bar functions
+		
+//Hamburger menu
+$("#hamburger_menu_button").click(function(){
+  $("#hamburger_menu_container").toggle();
+  return false;
+});
+
+$("body:not(#hamburger_menu_button, #hamburger_menu_container)").click(function(){
+  $('#hamburger_menu_container').hide();
+});
+
+//Search
+$("#search_form").submit(function(){
+  //Get the value from the bar
+  var term = $("#search").val();
+  
+  var nonce = getNonce();	
+  if( !isBlank( term ) ){
+    //Send to the back end
+    $.ajax({
+      type: "POST",
+      async: false,
+      url: '/search/record',
+      data: { 'term':term, 'nonce':nonce },
+      dataType: "json",
+      success: function( response, status, xhr ){
+        //Don't care, go on to search page
+        //Go on to search page, items by default
+        term = encodeURIComponent( term );
+        term = term.replace( /%20/g, '+' );
+        goTo( '/search/items/'+term );
+        console.log( response );
+      },
+      error: function( xhr, status, error ){
+        //Don't care, go on to search page
+        //Go on to search page, items by default
+        term = encodeURIComponent( term );
+        //goTo( '/search/items/'+term );
+        console.log(xhr);
+      }
+    });
+    
+    
+    
+  }
+  
+  return false; //Prevent refresh
+  
+});
+
+//Logged out buttons
+$("button#login").click(function(){
+  goTo('/login');
+});
+
+$("button#join").click(function(){
+  showRequestInvite();
+});
+
+//Activity button
+$("#activity_menu_button").click(function(){
+  goTo('/activity');
+});
+
+//User menu
+$("#user_menu_button").click(function(){
+  $("#user_menu_container").toggle();
+  return false;
+});
+
+$("body:not(#user_menu_container, #user_menu_button)").click(function(){
+  $('#user_menu_container').hide();
+});
