0ffa3b2e447703ec9c5c0247983d8c1eaddf14fd
Jaime Rump <rumpj2@nku.edu>
2014-04-04 20:02:33 +0000

    Implement user registration functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,42 @@
 class UsersController<ApplicationController
+
+  skip_before_action :require_login, only: [:new, :create]
+  
+  # Create
+  
+  def new
+    # Make sure they're not already logged in
+    @current_user = current_user
+    if( @current_user != nil )
+      redirect_to users_path
+    end
+      
+    @user = User.new
+  end
+  
+  def create
+    
+    # Make sure they're not already logged in
+    @current_user = current_user
+    if( @current_user != nil )
+      redirect_to users_path
+    end
+     
+    @user = User.new(user_params)
+    if @user.save
+        
+      # Log them in
+      session[:user_id] = @user.id
+        
+      redirect_to users_path, notice: "User successfully added!"
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  
+  validates :email, uniqueness: true
 
   def full_name
     "#{first_name} #{last_name}"
